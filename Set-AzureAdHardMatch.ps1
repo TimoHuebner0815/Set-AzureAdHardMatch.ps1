02e0b6963c26bcb8203912027f961679231f7c52
Timo Hübner <80783501+TimoHuebner0815@users.noreply.github.com>
2022-08-01 11:14:23 +0200

    Update Set-AzureAdHardMatch.ps1

--- Set-AzureAdHardMatch.ps1
+++ Set-AzureAdHardMatch.ps1
@@ -1,47 +1,58 @@
-# .PARAMETER User
-# AD DS User SamAccountName
-# .PARAMETER GUID
+# .PARAMETER ObjectGUID
 # AD DS User ObjectGUID
 # .PARAMETER UserPrincipalName
 # AD DS User UserPrincipalName
+# .PARAMETER MSOnlineDomain
+# M365 Domain
 
 Param
 (
     [Parameter(Mandatory=$true)]
-    [String]$User,
-    [GUID]$GUID,
-    [String]$UserPrincipalName
+    [GUID]$ObjectGUID,
+    [String]$UserPrincipalName,
+    [Parameter(Mandatory=$false)]
+    [String]$MSOnlineDomain = 'MsOnline Domain'
 )
 
-#Import Module
-Import-Module MSOnline
-
-#Check for MSOL Session is still opne
+#Check for MSOL Session is still open
 Function MSOLConnected {
     Get-MsolDomain -ErrorAction SilentlyContinue | out-null
     return $?
 }
 
-#Convert GUID into Base64
+#Convert ObjectGUID into Base64GUID
 Function Convert-GUIDtoBase64 
 {
     Param
     (
-        [GUID]$GUID
+        [ObjectGUID]$ObjectGUID
     )
 
-    $Base64GUID = [system.convert]::ToBase64String($GUID.ToByteArray())
+    $Base64GUID = [system.convert]::ToBase64String($ObjectGUID.ToByteArray())
     
     Write-Output $Base64GUID
 }
 
-$Base64GUID = Convert-GUIDtoBase64 -GUID $GUID
-
+#Import Module
+Import-Module MSOnline
 #Connect to MSOL if not Connectet
 if (-not (MSOLConnected)) 
 {
     Connect-MsolService
 }
 
+#Convert ObjectGUID into Base64GUID using Funktion
+$Base64GUID = Convert-GUIDtoBase64 -ObjectGUID $ObjectGUID
+
 #Set ImmutableId to MSOL User
-Set-MsolUser -UserPrincipalName $UserPrincipalName -ImmutableId $Base64GUID -Verbose
+try
+{
+        Set-MsolUser -UserPrincipalName $UserPrincipalName -ImmutableId $Base64GUID -Verbose -ErrorAction Stop
+}
+catch 
+{
+    $MSOnlineUser = $UserPrincipalName.Split('@')[0] + '@' +$MSOnlineDomain
+
+    Set-MsolUser -UserPrincipalName $MSOnlineUser -ImmutableId $Base64GUID -Verbose
+
+}
